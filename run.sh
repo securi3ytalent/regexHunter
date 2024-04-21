9515197664ac1c18a34deec5cd9d0179ddcc3ba3
securi3ytalent <devmehedi101@gmail.com>
2024-04-21 08:12:55 -0700

    Domain to IP argument added

--- run.sh
+++ run.sh
@@ -25,21 +25,17 @@ echo -e "${YELLOW}join_us: https://t.me/Securi3yTalent${NC}"
 echo " "
 
 
-
-
 # Function to display usage message
 display_usage() {
-
     echo "Usage: $0 <input_file1> [<input_file2> ...] ** Fast need to sorting url's"
-    echo "Usage: $0 [-h] [-t] [-s]      example:  run.sh -t | run.sh -s "
-
-    echo " "
+    echo "Usage: $0 [-h] [-t] [-s] [-i]      example:  run.sh -t | run.sh -s | run.sh -i"
+    echo ""
     echo "Options:"
     echo "  -h, --help       Display this help message"
     echo "  -t               Identify the technology used by websites"
     echo "  -s               Find the endpoint sensitive data"
+    echo "  -i               Convert domains to IP addresses and save to file"
 }
-
 
 # Check if the script was called with the -h argument
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
@@ -91,6 +87,50 @@ if [[ "$1" == "-s" ]]; then
     shift
     # Run the Node.js script tools.js
     node tools.js
+    exit 0
+fi
+
+# Check if the script was called with the -i argument
+if [[ "$1" == "-i" ]]; then
+    # Shift the arguments to remove the -i option
+    shift
+    # Python code to convert domains to IP addresses and save to file
+    python3 <<END
+import socket
+from urllib.parse import urlparse
+
+def get_domain_from_url(url):
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
+    return domain
+
+def get_ip_from_domain(domain):
+    try:
+        ip_address = socket.gethostbyname(domain)
+        return ip_address
+    except socket.gaierror:
+        return None
+
+def main():
+    file_path = "domainlist.txt"
+    unique_ips = set()
+
+    with open(file_path, "r") as file:
+        urls = file.readlines()
+    
+    with open("IP_list.txt", "w") as ip_file:
+        for url in urls:
+            url = url.strip()  # Remove leading/trailing whitespace and newline characters
+            domain = get_domain_from_url(url)
+            ip_address = get_ip_from_domain(domain)
+            if ip_address and ip_address not in unique_ips:
+                unique_ips.add(ip_address)
+                print(ip_address)
+                ip_file.write(ip_address + "\n")
+
+if __name__ == "__main__":
+    main()
+END
     exit 0
 fi
 
